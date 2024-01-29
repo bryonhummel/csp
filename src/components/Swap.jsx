7af17004e241c4df487a0fd2d4c0a7c686643081
Bryon Hummel <bryonhummel@gmail.com>
2024-01-29 17:41:41 -0500

    simple swap cards

--- src/components/Swap.jsx
+++ src/components/Swap.jsx
@@ -1,23 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
-function SwapEdit({toggleEditMode}) {
-    return(
-        <div onClick={toggleEditMode}>swap edit</div>
-    )
-}
-
-function PatrollerName({teamNumber, teamLetter, firstName, lastName, isFrom}) {
+function PatrollerNameBadge({patroller, isFrom}) {
     const fromToStr = isFrom ? 'From' : 'To'
     return(
-        <div className=''>
-            <div className='border rounded-lg flex flex-row items-center'>
-                <div className='min-w-12 bg-gray-200 rotate-90'>
-                    <div className=''>{fromToStr}</div>
-                </div>
+        <div className='flex-1 rounded-lg shadow'>
+            <div className='rounded-t-lg bg-gray-200'>
+                <div className='uppercase text-gray-500'>{fromToStr}</div>
+            </div>
+            <div className='border rounded-b-lg flex flex-row items-center'>
                 <div className='flex-1 flex items-center'>
-                    <div className='border-r p-1'>{teamNumber}{teamLetter}</div>
-                    <div className='flex-1 px-4'>
-                        <span>{firstName}<br/>{lastName}</span>
+                    <div className='border-r p-1 uppercase'>{patroller.team}{patroller.letter}</div>
+                    <div className='flex-1 px-4 py-1 font-bold'>
+                        <span>{patroller.firstName}<br/>{patroller.lastName}</span>
                     </div>
                 </div>
             </div>
@@ -25,38 +19,99 @@ function PatrollerName({teamNumber, teamLetter, firstName, lastName, isFrom}) {
     )
 }
 
-function SwapView({toggleEditMode}) {
+function PatrollerSwap() {
     return(
-        <div onClick={toggleEditMode}>
-            <div className='flex flex-row items-center m-4'>
-                <div>
-                    <span>Fri</span>
-                    <span className='px-2 text-gray-300 '>|</span>
-                    <span>Feb 10</span>
-                </div>
-                <div className='flex-1' />
-                <div className='rounded-lg bg-gray-50 border font-bold px-4 py-1 shadow-sm'>Team 2</div>
+        <div className='flex-1 rounded-lg shadow'>
+            <div className='rounded-t-lg bg-yellow-400'>
+                <div className='uppercase text-black'>to</div>
             </div>
-            <div>18:30 - Close</div>
+            <div className='border rounded-b-lg flex flex-row items-center border-yellow-300'>
+                <div className='flex-1 flex items-center bg-yellow-300'>
+                    <div className='flex-1 px-4 py-1 font-bold'>
+                        <span className=''>Need<br/>Swap</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const dayString = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+]
+const monthString = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
+
+function isDateToday(date) {
+    const today = new Date()
+    return date.getDate() == today.getDate() &&
+      date.getMonth() == today.getMonth() &&
+      date.getFullYear() == today.getFullYear()
+}
+
+function SwapView({info}) {
+    const dayOfWeek = dayString[info.shiftInfo.date.getDay()]
+    const month = monthString[info.shiftInfo.date.getMonth()]
+    const date = info.shiftInfo.date.getDate()
+
+    const isToday = isDateToday(info.shiftInfo.date)
+
+    function dateDisplay() {
+        if (isToday) {
+            return(
+                <div className='uppercase border rounded px-4 py-0.5 bg-green-700 border-green-700 text-white font-bold'>Today</div>
+            )
+        } else {
+            return(
+            <div className='uppercase'>
+                <span className='font-bold'>{dayOfWeek}</span>
+                <span className='px-2 text-gray-300 '>|</span>
+                <span>{month} {date}</span>
+            </div>)
+        }
+    }
+    
+    return(
+        <div>
+            <div className='flex flex-row items-center m-4'>
+                {dateDisplay()}
+                <div className='flex-1' />
+                <div className='uppercase font-bold '>Team {info.shiftInfo.team}</div>
+            </div>
+            <div>{info.shiftInfo.hours}</div>
             <div className='flex flex-wrap items-center m-4 justify-center gap-4'>
-                <PatrollerName teamNumber="2" teamLetter="F" firstName="Bryon" lastName="Hummel" isFrom={true} />
-                <PatrollerName teamNumber="2" teamLetter="F" firstName="Some" lastName="other" />
+                <PatrollerNameBadge patroller={info.fromPatroller} isFrom={true} />
+                { info.toPatroller && <PatrollerNameBadge patroller={info.toPatroller} isFrom={false} /> }
+                { !info.toPatroller && <PatrollerSwap /> }
             </div>
         </div>
     )
 }
 
 
-function Swap() {
-    const [isEditMode, setIsEditMode] = useState(false)
 
-    const toggleEditMode = () => {
-        setIsEditMode(!isEditMode === true)
-    }
+function Swap({swapInfo}) {
     return(
         <div className='border bg-white rounded-2xl'>
-            { isEditMode && <SwapEdit toggleEditMode={toggleEditMode} /> }
-            { !isEditMode && <SwapView toggleEditMode={toggleEditMode} /> }
+            <SwapView info={swapInfo} />
         </div>
     )
 }
--- src/routes/swaps.lazy.tsx
+++ src/routes/swaps.lazy.tsx
@@ -6,12 +6,70 @@ import Swap from "../components/Swap"
 export const Route = createLazyFileRoute('/swaps')({
     component: Swaps,
   })
+
+  var swapList = [
+    {
+        fromPatroller: {
+            firstName: "Bryon",
+            lastName: "Hummel",
+            team: "4",
+            letter: "b"
+        },
+        toPatroller: {
+            firstName: "Mike",
+            lastName: "Sutherland",
+            team: "4",
+            letter: "a"
+        },
+        shiftInfo: {
+            date: new Date(Date.parse("2024-01-28T00:00:00")),
+            team: 4,
+            hours: "18:30 - Close",
+        }
+    },
+    {
+        fromPatroller: {
+            firstName: "Brigitte",
+            lastName: "DeJong",
+            team: "4",
+            letter: "h"
+        },
+        toPatroller: {
+            firstName: "Bryon",
+            lastName: "Hummel",
+            team: "4",
+            letter: "b"
+        },
+        shiftInfo: {
+            date: new Date(Date.parse("2024-01-29T00:00:00")),
+            team: 4,
+            hours: "18:30 - Close",
+        }
+    },
+    {
+        fromPatroller: {
+            firstName: "Bryon",
+            lastName: "Hummel",
+            team: "4",
+            letter: "b"
+        },
+        shiftInfo: {
+            date: new Date(Date.parse("2024-02-04T00:00:00")),
+            team: 4,
+            hours: "18:30 - Close",
+        }
+    }
+  ]
   
   function Swaps() {
     return (
       <div className="m-2 text-center">
         <h3>Swap Log</h3>
-        <Swap />
+        <div className="grid gap-2 md:grid-cols-2">
+            <Swap swapInfo={swapList[0]} />
+            <Swap swapInfo={swapList[1]} />
+            <Swap swapInfo={swapList[2]} />
+        </div>
       </div>
     )
   }
