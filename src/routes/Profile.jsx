3e5dd35e114ce21e13783d8fb6e6ad66dc6c572e
Bryon Hummel <bryonhummel@gmail.com>
2024-02-02 18:16:40 -0500

    add some placeholder link buttons to profile

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,6 +1,26 @@
 import { Link } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 
+function PlaceholderNavLink({ path, logout }) {
+    if (path === 'logout') {
+        return (
+            <Link onClick={logout}>
+                <div className="m-2 cursor-pointer rounded-lg bg-red-600 p-2 text-white active:bg-red-400">
+                    Logout
+                </div>
+            </Link>
+        )
+    } else {
+        return (
+            <Link className="uppercase" to={'/' + path}>
+                <div className="m-2 cursor-pointer rounded-lg bg-red-600 p-2 text-white  active:bg-red-400">
+                    {path}
+                </div>
+            </Link>
+        )
+    }
+}
+
 function Profile() {
     const { user, logout } = useAuth()
 
@@ -8,8 +28,9 @@ function Profile() {
         <div>
             <h1>Hello {user.email}</h1>
 
-            <Link to={'/swaps'}>Swaps</Link>
-            <Link onClick={logout}>Logout</Link>
+            <PlaceholderNavLink path="roster" />
+            <PlaceholderNavLink path="swaps" />
+            <PlaceholderNavLink path="logout" logout={logout} />
         </div>
     )
 }
