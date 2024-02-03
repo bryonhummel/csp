981e1ae68f31697a83a7ee1a0763e265f1afee23
Bryon Hummel <bryonhummel@gmail.com>
2024-02-02 22:42:05 -0500

    remove unused fetch from db

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 import TopNav from '../components/TopNav'
 import { useAuth } from '../hooks/useAuth'
-import { fetchTeams } from '../supabase/client'
 
 function Root() {
     const { user } = useAuth()
-    const [teamList, setTeamList] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await fetchTeams()
-            setTeamList(data)
-        }
-        fetchData()
-    }, [user])
 
     return (
         <div>
--- src/supabase/client.js
+++ src/supabase/client.js
@@ -5,21 +5,6 @@ const projectKey = import.meta.env.VITE_SUPABASE_PROJECT_ANON_KEY
 
 export const supabase = createClient(projectURL, projectKey)
 
-// returns a list of team IDs to prove we can access the db
-export async function fetchTeams() {
-    const { data, error } = await supabase.from('teams').select('team_id')
-
-    if (error) {
-        console.error('Error fetching data:', error)
-        return []
-    }
-
-    return data.map((item) => item.team_id)
-}
-
-// return a data structure containing all the 'static' information (roster/team structure)
-export async function getRosterInformation() {}
-
 export async function fetchRoster() {
     console.log('supabase.client fetchRoster')
     const { data, error } = await supabase.from('roster').select('*')
