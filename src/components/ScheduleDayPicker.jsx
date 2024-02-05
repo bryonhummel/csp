7a1891c77e551d58fa3cba8dc20d76c1a454a407
Bryon Hummel <bryonhummel@gmail.com>
2024-02-05 18:19:16 -0500

    partially date selectable

--- src/components/ScheduleDayPicker.jsx
+++ src/components/ScheduleDayPicker.jsx
@@ -1,8 +1,22 @@
 import { useState } from 'react'
-import { DAY_STRING_2CH_MAP, isDateToday } from '../utils/schedUtils'
+import {
+    DAY_STRING_2CH_MAP,
+    MONTH_STRING_MAP,
+    isDateToday,
+} from '../utils/schedUtils'
+
+function getLastSunday(d, offset = 0) {
+    //var t = new Date(d)
+    //var t = new Date('2024-02-03T00:00:00')
+    var t = new Date('2024-02-05T00:00:00')
+    t.setDate(t.getDate() - t.getDay() + offset)
+    return t
+}
 
 function ScheduleDayPicker({ onDateChange }) {
     const [selected, setSelected] = useState(0)
+
+    const [selectedDate, setSelectedDate] = useState(new Date())
 
     function DayButton({
         dayStr,
@@ -48,39 +62,42 @@ function ScheduleDayPicker({ onDateChange }) {
         )
     }
 
-    // hack... need to figure out how i'm doing this for real
-    const months = ['Feb', 'Feb', 'Feb', 'Feb', 'Feb', 'Feb', 'Feb']
-    // hack... need to figure out how i'm doing this for real
-    const dates = [4, 5, 6, 7, 8, 9, 10]
-    const hasEvent = [4, 8, 10]
-
+    // weird hack cuz i can't think of abetter way to do this...
+    // get last sunday... offset by 1 (last saturday)
+    // we will iterate below with a pre-incrememnt so we start the week on sunday
+    const sundayDate = getLastSunday(selectedDate)
+    var d = getLastSunday(selectedDate, -1)
+    var prevMon = null
     return (
         <div className="flex rounded-lg bg-gray-200 shadow">
             {DAY_STRING_2CH_MAP.map((day, idx) => {
+                // loop through the week starting at sunday
+                d.setDate(d.getDate() + 1)
+                var displayMonth = ''
+                if (prevMon != d.getMonth() || true) {
+                    prevMon = d.getMonth()
+                    displayMonth = MONTH_STRING_MAP[d.getMonth()]
+                }
                 return (
                     <DayButton
                         onClick={() => {
+                            // BRYON TODO: kinda hacky state maintenance here... get rid of idx maybe?
                             setSelected(idx)
+                            const selected = new Date(sundayDate)
+                            selected.setDate(sundayDate.getDate() + idx)
                             // hack for date change
-                            onDateChange(
-                                '2024-02-' + String(dates[idx]).padStart(2, '0')
-                            )
+                            setSelectedDate(selected)
+                            onDateChange(selected.toISOString().split('T')[0])
                         }}
                         key={idx}
                         dayStr={day}
-                        month={months[idx]}
-                        day={dates[idx]}
+                        month={displayMonth}
+                        day={d.getDate()}
                         first={idx == 0}
                         last={idx == 6}
                         active={idx == selected}
-                        scheduled={hasEvent.includes(dates[idx])}
-                        isToday={isDateToday(
-                            new Date(
-                                '2024-02-' +
-                                    String(dates[idx]).padStart(2, '0') +
-                                    'T00:00:00'
-                            )
-                        )}
+                        scheduled={idx == 4} // Bryon TODO: need to create helper to know what MY schedule is
+                        isToday={isDateToday(d)}
                     />
                 )
             })}
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -6,7 +6,7 @@ function TopNav() {
     const { user, logout } = useAuth()
 
     return (
-        <div className="fixed top-0 flex h-12 w-full flex-grow items-center justify-evenly bg-red-600 text-white shadow-md">
+        <div className="fixed top-0 flex h-14 w-full flex-grow items-center justify-evenly bg-red-600 text-white shadow-md">
             <Link to={'/members/profile'}>
                 <Logo />
             </Link>
