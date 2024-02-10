13fb1ebab8248a38e9a6811929a4d8b16888800f
Bryon Hummel <bryonhummel@gmail.com>
2024-02-10 17:24:43 -0500

    allow filtering of swaps

--- src/routes/Swaps.jsx
+++ src/routes/Swaps.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import Swap from '../components/Swap'
 import { useSchedule } from '../hooks/useSchedule'
 import { SHIFT_STRING_MAP } from '../utils/schedUtils'
 import { useRoster, getRosterEntry } from '../hooks/useRoster'
 
+function FilterToggle({ toggleValue, setToggle }) {
+    return (
+        <div className="ml-auto">
+            <label class="relative me-5 inline-flex cursor-pointer items-center">
+                <input
+                    type="checkbox"
+                    value=""
+                    class="peer sr-only"
+                    checked={toggleValue}
+                    onClick={() => setToggle(!toggleValue)}
+                />
+                <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-red-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-red-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-red-800"></div>
+                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                    Show All
+                </span>
+            </label>
+        </div>
+    )
+}
+
 function Swaps() {
     const { swaps } = useSchedule()
     const { roster } = useRoster()
+    const [displayAllSwaps, setDisplayAllSwaps] = useState(false)
 
     var swap_list = []
 
@@ -52,7 +74,12 @@ function Swaps() {
                             letter: swap.to_letter,
                         }
                     }
-                    swap_list.push(<Swap key={key} swapInfo={swapInfo} />)
+                    if (
+                        displayAllSwaps ||
+                        !(swap.to_team_number && swap.to_letter)
+                    ) {
+                        swap_list.push(<Swap key={key} swapInfo={swapInfo} />)
+                    }
                 })
             })
         })
@@ -62,6 +89,10 @@ function Swaps() {
         <div className=" mx-auto my-2 max-w-4xl text-center">
             <div className="mx-2 grid gap-2 md:grid-cols-2">
                 <h1 className="text-lg font-bold">Shift Swaps</h1>
+                <FilterToggle
+                    toggleValue={displayAllSwaps}
+                    setToggle={setDisplayAllSwaps}
+                />
                 {swap_list}
             </div>
         </div>
